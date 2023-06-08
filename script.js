058715eef9e00c2552d707fd6bfe8fce81d3f871
Oleg Tarasenko <gryzovick2@gmail.com>
2023-06-08 22:27:59 +0300

    // all info

---
+++ script.js
@@ -0,0 +1,157 @@
+// const isUserLoggedIn = true;
+
+// if else opreator
+
+// if (isUserLoggedIn) {
+//     alert("You are logged in!");
+// } else {
+//     alert("Your are not logged in!");
+// }
+
+// Tearnary operator
+
+// isUserLoggedIn ? alert("You are logged in!") : "Your are not logged in!";
+
+// Switch operator
+
+// const age = 10;
+
+// switch (age) {
+//     case 10:
+//         alert("Happy birthday, give a kiss!");
+//         break;
+//     case 20:
+//         alert("Happy birthday, give a kisee!");
+//         break;
+//     case 30:
+//         alert("Happy birthday, give me grandchildren!");
+//         break;
+//     default:
+//         alert("Happy birthday, give me yoru legacy!");
+// }
+
+
+// const num1 = 20;
+// const num2 = 5;
+// let num3 = 50;
+// num3 += 30;  // num3 = num3 + 30;
+// console.log(num3);
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+
+// const isUserLoggedIn = true;
+// const age = 18;
+// const name = 'Vitaly';
+
+// if (age >= 18) {
+//     alert("Congrats");
+// }
+
+// const isUserLoggedIn = true;
+// const age = 18;
+// const name = 'Vitaly';
+
+// if (age !== 18) {
+//     alert("Congrats");
+// }
+
+// const isUserLoggedIn = true;
+// const age = 18;
+// const name = 'Vitaly';
+
+// if (age === 18 && name === 'Vitaly' && !isUserLoggedIn) {
+//     alert("Congrats");
+// }
+
+// console.log('my' + 'string');
+
+
+// const obj = {
+//     name: 'Vitality',
+//     age: 18,
+//     sex: 'male'
+// }
+// delete obj.age;
+
+// console.log(obj)
+// console.log('sex' in obj)
+
+//  Type coercion
+
+// console.log('fkljkldgkldfg')
+
+// Function
+
+// const userName = 'Vitaly';
+// function sayHello(name) {
+//     alert('Hello, dear ' + name)
+// }
+
+// sayHello(userName);
+
+// function createUserName() {
+//     return 'Vitaly';
+// }
+
+// const userName = createUserName();
+// console.log(userName);
+
+// const userName = 'Vitaly';
+
+// function createUserName(name) {
+//     const result = name.toUpperCase();
+//     return result;
+// }
+
+// // const userName = createUserName();
+// console.log(createUserName(userName));
+
+//  Function
+
+// Function Declaration
+
+// function sayHello(greetings) {
+//     alert(greetings);
+// }
+
+// sayHello('bonjour');
+
+// // Function Expression
+
+// const sayHello2 = function(greetings) {
+//     alert(greetings);
+// }
+
+// sayHello2('Czrszcz');
+
+// function returnSmth() {
+//     console.log(1);
+//     console.log(1);
+//     return 'hello'
+//     console.log(1);
+//     console.log(1);
+// }
+// console.log(returnSmth()); const greeting  дорівнюе hello
+
+// const car = {
+//     speed: 100,
+//     color: 'red',
+//     drive: function () {
+//         console.log('I drive very fast')
+//     }
+// }
+
+// car.drive()
+
+function sayHello() {
+    return 'hello'
+}
+
+const test = function () {
+    
+}
